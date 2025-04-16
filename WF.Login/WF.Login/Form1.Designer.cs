8ac9b5bd5148054cbf0eac840199de2719cbf0ad
Marcelo Ballin <puffersenac@gmail.com>
2025-04-15 21:34:04 -0300

    .

--- WF.Login/WF.Login/Form1.Designer.cs
+++ WF.Login/WF.Login/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
+            pictureBox1 = new PictureBox();
             Exit = new PictureBox();
             Minimize = new PictureBox();
             LoginGroup = new GroupBox();
             lnk_recovery = new LinkLabel();
+            btn_login = new Button();
+            txt_password = new TextBox();
+            txt_username = new TextBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Exit).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Minimize).BeginInit();
             LoginGroup.SuspendLayout();
@@ -42,7 +47,7 @@
             // 
             // panel1
             // 
-            panel1.BackColor = SystemColors.WindowFrame;
+            panel1.BackColor = SystemColors.HotTrack;
             panel1.Controls.Add(pictureBox1);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
@@ -50,90 +55,107 @@
             panel1.Size = new Size(320, 450);
             panel1.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-110, -73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(540, 580);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
             // Exit
             // 
-            Exit.Location = new Point(766, 4);
+            Exit.BackColor = Color.Firebrick;
+            Exit.Image = (Image)resources.GetObject("Exit.Image");
+            Exit.Location = new Point(770, 0);
             Exit.Name = "Exit";
             Exit.Size = new Size(30, 30);
+            Exit.SizeMode = PictureBoxSizeMode.Zoom;
             Exit.TabIndex = 2;
             Exit.TabStop = false;
+            Exit.Click += Exit_Click;
             // 
             // Minimize
             // 
-            Minimize.Location = new Point(730, 4);
+            Minimize.BackColor = Color.Teal;
+            Minimize.Image = (Image)resources.GetObject("Minimize.Image");
+            Minimize.Location = new Point(739, 0);
             Minimize.Name = "Minimize";
             Minimize.Size = new Size(30, 30);
+            Minimize.SizeMode = PictureBoxSizeMode.Zoom;
             Minimize.TabIndex = 3;
             Minimize.TabStop = false;
             // 
             // LoginGroup
             // 
             LoginGroup.BackColor = Color.Black;
+            LoginGroup.CausesValidation = false;
             LoginGroup.Controls.Add(lnk_recovery);
             LoginGroup.Controls.Add(btn_login);
             LoginGroup.Controls.Add(txt_password);
             LoginGroup.Controls.Add(txt_username);
-            LoginGroup.Location = new Point(335, 50);
+            LoginGroup.FlatStyle = FlatStyle.Flat;
+            LoginGroup.Font = new Font("Segoe UI", 16F);
+            LoginGroup.ForeColor = SystemColors.ButtonFace;
+            LoginGroup.Location = new Point(338, 40);
             LoginGroup.Name = "LoginGroup";
             LoginGroup.Size = new Size(450, 340);
             LoginGroup.TabIndex = 4;
             LoginGroup.TabStop = false;
-            LoginGroup.Text = "groupBox1";
+            LoginGroup.Text = "Login";
             // 
-            // txt_username
+            // lnk_recovery
             // 
-            txt_username.BackColor = Color.Black;
-            txt_username.BorderStyle = BorderStyle.FixedSingle;
-            txt_username.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txt_username.ForeColor = SystemColors.Info;
-            txt_username.Location = new Point(42, 96);
-            txt_username.Name = "txt_username";
-            txt_username.Size = new Size(390, 30);
-            txt_username.TabIndex = 2;
-            txt_username.Text = "Usuário";
+            lnk_recovery.AutoSize = true;
+            lnk_recovery.Font = new Font("Segoe UI", 10F);
+            lnk_recovery.Location = new Point(293, 185);
+            lnk_recovery.Name = "lnk_recovery";
+            lnk_recovery.Size = new Size(139, 23);
+            lnk_recovery.TabIndex = 0;
+            lnk_recovery.TabStop = true;
+            lnk_recovery.Text = "Recuperar Senha";
+            // 
+            // btn_login
+            // 
+            btn_login.BackColor = Color.DarkSeaGreen;
+            btn_login.Font = new Font("Microsoft Sans Serif", 20F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_login.ForeColor = SystemColors.ActiveCaptionText;
+            btn_login.Location = new Point(142, 238);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(177, 61);
+            btn_login.TabIndex = 3;
+            btn_login.Text = "Entrar";
+            btn_login.UseVisualStyleBackColor = false;
             // 
             // txt_password
             // 
             txt_password.BackColor = Color.Black;
             txt_password.BorderStyle = BorderStyle.FixedSingle;
-            txt_password.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_password.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txt_password.ForeColor = SystemColors.Info;
             txt_password.Location = new Point(42, 148);
             txt_password.Name = "txt_password";
-            txt_password.Size = new Size(390, 30);
-            txt_password.TabIndex = 3;
+            txt_password.Size = new Size(390, 34);
+            txt_password.TabIndex = 2;
             txt_password.Text = "Senha";
+            txt_password.Enter += txt_password_Enter;
+            txt_password.Leave += txt_password_Leave;
             // 
-            // btn_login
+            // txt_username
             // 
-            btn_login.BackColor = SystemColors.HotTrack;
-            btn_login.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_login.Location = new Point(142, 238);
-            btn_login.Name = "btn_login";
-            btn_login.Size = new Size(177, 61);
-            btn_login.TabIndex = 4;
-            btn_login.Text = "Login";
-            btn_login.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-20, 24);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(340, 400);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
-            // lnk_recovery
-            // 
-            lnk_recovery.AutoSize = true;
-            lnk_recovery.Location = new Point(356, 181);
-            lnk_recovery.Name = "lnk_recovery";
-            lnk_recovery.Size = new Size(76, 20);
-            lnk_recovery.TabIndex = 5;
-            lnk_recovery.TabStop = true;
-            lnk_recovery.Text = "linkLabel1";
+            txt_username.BackColor = Color.Black;
+            txt_username.BorderStyle = BorderStyle.FixedSingle;
+            txt_username.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_username.ForeColor = SystemColors.Info;
+            txt_username.Location = new Point(42, 96);
+            txt_username.Name = "txt_username";
+            txt_username.Size = new Size(390, 34);
+            txt_username.TabIndex = 1;
+            txt_username.Text = "Usuário";
+            txt_username.Enter += txt_username_Enter;
+            txt_username.Leave += txt_username_Leave;
             // 
             // Form1
             // 
@@ -149,6 +171,7 @@
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)Exit).EndInit();
             ((System.ComponentModel.ISupportInitialize)Minimize).EndInit();
             LoginGroup.ResumeLayout(false);
--- WF.Login/WF.Login/Form1.cs
+++ WF.Login/WF.Login/Form1.cs
@@ -7,14 +7,40 @@ namespace WF.Login
             InitializeComponent();
         }
 
-        private void Exit_Click(object sender, EventArgs e)
+        private void txt_username_Enter(object sender, EventArgs e)
         {
-
+            if (txt_username.Text == "Usuário")
+                txt_username.Text = string.Empty;
+        }
+        // string.Empty é o mesmo que " "
+        private void txt_username_Leave(object sender, EventArgs e)
+        {
+            if (txt_username.Text == string.Empty)
+                txt_username.Text = "Usuário";
         }
 
-        private void Minimize_Click(object sender, EventArgs e)
+        private void txt_password_Enter(object sender, EventArgs e)
         {
+            if (txt_password.Text == "Senha")
+                txt_password.Text = string.Empty;
+        }
 
+        private void txt_password_Leave(object sender, EventArgs e)
+        {
+            if (txt_password.Text == string.Empty)
+                txt_password.Text = "Senha";
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            var sair = MessageBox.Show("Quer realmente sair? Dados não salvos serão perdidos.",
+                "Sair",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (sair == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
     }
 }
