c6251c1328a42fd0b939d25f7e33f4a8e0f57e6b
Marcelo Ballin <puffersenac@gmail.com>
2025-04-22 20:22:39 -0300

    Criada a base dos forms

--- WF.Login/WF.Login/FormPagamento.Designer.cs
+++ WF.Login/WF.Login/FormPagamento.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WF.Login
 {
-    partial class form_Home
+    partial class form_Pagamento
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_Home));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_Pagamento));
             panel1 = new Panel();
             gpb_Info = new GroupBox();
             lbl_Version = new Label();
@@ -41,20 +41,12 @@
             btn_Pedidos = new Button();
             pictureBox1 = new PictureBox();
             panel2 = new Panel();
-            gpb_SocialMedia = new GroupBox();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
             lbl_AppTitle = new Label();
             panel1.SuspendLayout();
             gpb_Info.SuspendLayout();
             gpb_Buttons.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
-            gpb_SocialMedia.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -115,7 +107,6 @@
             // button6
             // 
             button6.BackColor = Color.DodgerBlue;
-            button6.FlatAppearance.MouseDownBackColor = Color.Cyan;
             button6.FlatStyle = FlatStyle.Flat;
             button6.Font = new Font("Malgun Gothic", 12F);
             button6.Image = (Image)resources.GetObject("button6.Image");
@@ -144,8 +135,8 @@
             // 
             // btn_Pagamentos
             // 
-            btn_Pagamentos.BackColor = Color.DodgerBlue;
-            btn_Pagamentos.FlatAppearance.MouseDownBackColor = Color.Cyan;
+            btn_Pagamentos.BackColor = Color.Cyan;
+            btn_Pagamentos.FlatAppearance.MouseDownBackColor = Color.DodgerBlue;
             btn_Pagamentos.FlatStyle = FlatStyle.Flat;
             btn_Pagamentos.Font = new Font("Malgun Gothic", 12F);
             btn_Pagamentos.Image = (Image)resources.GetObject("btn_Pagamentos.Image");
@@ -160,7 +151,6 @@
             // btn_Clientes
             // 
             btn_Clientes.BackColor = Color.DodgerBlue;
-            btn_Clientes.FlatAppearance.MouseDownBackColor = Color.Cyan;
             btn_Clientes.FlatStyle = FlatStyle.Flat;
             btn_Clientes.Font = new Font("Malgun Gothic", 12F);
             btn_Clientes.Image = (Image)resources.GetObject("btn_Clientes.Image");
@@ -175,7 +165,6 @@
             // btn_Pedidos
             // 
             btn_Pedidos.BackColor = Color.DodgerBlue;
-            btn_Pedidos.FlatAppearance.MouseDownBackColor = Color.Cyan;
             btn_Pedidos.FlatStyle = FlatStyle.Flat;
             btn_Pedidos.Font = new Font("Malgun Gothic", 12F);
             btn_Pedidos.Image = (Image)resources.GetObject("btn_Pedidos.Image");
@@ -200,7 +189,6 @@
             // panel2
             // 
             panel2.BackColor = SystemColors.MenuHighlight;
-            panel2.Controls.Add(gpb_SocialMedia);
             panel2.Controls.Add(lbl_AppTitle);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(217, 0);
@@ -208,59 +196,17 @@
             panel2.Size = new Size(583, 74);
             panel2.TabIndex = 1;
             // 
-            // gpb_SocialMedia
-            // 
-            gpb_SocialMedia.Controls.Add(pictureBox4);
-            gpb_SocialMedia.Controls.Add(pictureBox3);
-            gpb_SocialMedia.Controls.Add(pictureBox2);
-            gpb_SocialMedia.Location = new Point(446, 3);
-            gpb_SocialMedia.Name = "gpb_SocialMedia";
-            gpb_SocialMedia.Size = new Size(125, 68);
-            gpb_SocialMedia.TabIndex = 2;
-            gpb_SocialMedia.TabStop = false;
-            gpb_SocialMedia.Text = "Social";
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(84, 26);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(32, 32);
-            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox4.TabIndex = 3;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(46, 26);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(32, 32);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 3;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(8, 26);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(32, 32);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
-            // 
             // lbl_AppTitle
             // 
             lbl_AppTitle.AutoSize = true;
             lbl_AppTitle.Font = new Font("Malgun Gothic", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbl_AppTitle.Location = new Point(62, 9);
+            lbl_AppTitle.Location = new Point(190, 15);
             lbl_AppTitle.Name = "lbl_AppTitle";
-            lbl_AppTitle.Size = new Size(293, 46);
+            lbl_AppTitle.Size = new Size(210, 46);
             lbl_AppTitle.TabIndex = 0;
-            lbl_AppTitle.Text = "Deliv-a-tron 5000";
+            lbl_AppTitle.Text = "Pagamentos";
             // 
-            // form_Home
+            // form_Pagamento
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -269,7 +215,7 @@
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "form_Home";
+            Name = "form_Pagamento";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Home";
             panel1.ResumeLayout(false);
@@ -279,10 +225,6 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            gpb_SocialMedia.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
@@ -297,12 +239,8 @@
         private Button btn_Pagamentos;
         private Button btn_Clientes;
         private Button btn_Pedidos;
-        private PictureBox pictureBox2;
         private Label lbl_AppTitle;
         private GroupBox gpb_Info;
-        private GroupBox gpb_SocialMedia;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
         private Label lbl_Version;
         private Label lbl_developed;
     }
